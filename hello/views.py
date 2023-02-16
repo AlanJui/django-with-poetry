5eeb622683e26696ca7779324d28fcf1c0a6f147
居正中 <AlanJui.1960@gmail.com>
2023-02-16 21:06:25 +0800

    變更 hello_there 網頁產出方式，改以 template 輸出 HTML 網頁內容。

--- hello/views.py
+++ hello/views.py
@@ -1,25 +1,36 @@
-import re
+# import re
 
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.utils.timezone import datetime
 
 
 def home(request):   # pyright: ignore
     return HttpResponse('Hello World')
 
-def hello_there(request, name):     # pyright: ignore
-    now = datetime.now()
-    formatted_now = now.strftime('%A, %d %B, %Y at %X')
+def hello_there(request, name):  # pyright: ignore
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now(),
+        },
+    )
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match('[a-zA-Z]+', name)
-
-    if match_object:
-        cleaned_name = match_object.group(0)
-    else:
-        cleaned_name = 'Friend'
-
-    # content = 'Hello there, {}! It is now {}'.format(cleaned_name, formatted_now)
-    content = f'Hello there, {cleaned_name}! It is now {formatted_now}'
-    return HttpResponse(content)
+# def hello_there(request, name):
+#     now = datetime.now()
+#     formatted_now = now.strftime('%A, %d %B, %Y at %X')
+#
+#     # Filter the name argument to letters only using regular expressions. URL arguments
+#     # can contain arbitrary text, so we restrict to safe characters only.
+#     match_object = re.match('[a-zA-Z]+', name)
+#
+#     if match_object:
+#         cleaned_name = match_object.group(0)
+#     else:
+#         cleaned_name = 'Friend'
+#
+#     # content = 'Hello there, {}! It is now {}'.format(cleaned_name, formatted_now)
+#     content = f'Hello there, {cleaned_name}! It is now {formatted_now}'
+#     return HttpResponse(content)
